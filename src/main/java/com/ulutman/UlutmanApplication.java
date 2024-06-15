a626a43348499e4ce28fc31aad2938a0507512b8
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-06-16 02:05:02 +0300

    create project ulutman

---
+++ src/main/java/com/ulutman/UlutmanApplication.java
@@ -0,0 +1,13 @@
+package com.ulutman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlutmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlutmanApplication.class, args);
+	}
+
+}
