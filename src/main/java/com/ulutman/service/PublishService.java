8db0902b452f38896927b13557e3e644ac910d27
aizirektoktosunova <aizirektosunova853@gmail.com>
2024-07-06 13:05:18 +0300

    added enam classes

--- src/main/java/com/ulutman/service/PublishService.java
+++ src/main/java/com/ulutman/service/PublishService.java
@@ -9,9 +9,7 @@ import com.ulutman.repository.PublishRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
