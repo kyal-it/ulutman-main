820af1be5d065e769561ba221fb21fea41a9300d
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-07-29 22:06:47 +0600

    finished  manage moderator

--- src/main/java/com/ulutman/repository/ComplaintRepository.java
+++ src/main/java/com/ulutman/repository/ComplaintRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
 @Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
 
--- src/main/java/com/ulutman/service/ManageComplaintService.java
+++ src/main/java/com/ulutman/service/ManageComplaintService.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
