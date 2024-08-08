a3e5cedbb6395f84d0ada55e1c42fb2966f72a8f
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-08-09 01:08:14 +0600

    integration  swagger into the project

--- src/main/java/com/ulutman/controller/AuthController.java
+++ src/main/java/com/ulutman/controller/AuthController.java
@@ -5,9 +5,12 @@ import com.ulutman.model.dto.AuthResponse;
 import com.ulutman.model.dto.LoginRequest;
 import com.ulutman.model.dto.LoginResponse;
 import com.ulutman.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +19,22 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 @Slf4j
+@Tag(name = "Auth")
+@SecurityRequirement(name = "Authorization")
 public class AuthController {
-    @Autowired
+
     private final AuthService authService;
 
+    @Operation(summary = "Create a new User")
+    @ApiResponse(responseCode = "201", description = "User created successfully")
     @PostMapping("/sign-up")
     public ResponseEntity<AuthResponse> save(@RequestBody AuthRequest authRequest) {
         AuthResponse response = authService.save(authRequest);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Login a new User")
+    @ApiResponse(responseCode = "201", description = "User login successfully")
     @PostMapping("/sign-in")
     public LoginResponse login(@RequestBody LoginRequest request) {
         return authService.login(request);
