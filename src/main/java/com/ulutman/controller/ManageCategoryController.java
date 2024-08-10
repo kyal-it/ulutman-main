d4230df826760450353c489bea69eca463f248d2
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-08-10 23:56:41 +0600

    update category and subCategory

--- src/main/java/com/ulutman/controller/ManageCategoryController.java
+++ src/main/java/com/ulutman/controller/ManageCategoryController.java
@@ -9,9 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @Slf4j
