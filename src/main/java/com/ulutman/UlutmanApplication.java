6fd1de2f076d2ebe74cfd33e941b2e9b38b9765b
duisheevvva <duisheeva.aizat@icloud.com>
2024-08-30 20:15:19 +0600

    deploy3.2

--- src/main/java/com/ulutman/UlutmanApplication.java
+++ src/main/java/com/ulutman/UlutmanApplication.java
@@ -2,14 +2,20 @@ package com.ulutman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class UlutmanApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(UlutmanApplication.class, args);
-		System.out.println("testfgsfgdsvfhfdsvhg");
+	}
+
+	@GetMapping
+	public String greetings(){
+		return "index";
 	}
 
 }
