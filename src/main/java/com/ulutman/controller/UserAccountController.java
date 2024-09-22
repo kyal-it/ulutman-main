3fcf34e399d0d2074f124ce6d3e5a28385d0374f
aizirektoktosunova <aizirektosunova853@gmail.com>
2024-09-22 23:10:44 +0600

    creat userProfilehj

--- src/main/java/com/ulutman/controller/UserAccountController.java
+++ src/main/java/com/ulutman/controller/UserAccountController.java
@@ -1,5 +1,7 @@
 package com.ulutman.controller;
 
+import com.ulutman.model.dto.UserAccountCreateRequest;
+import com.ulutman.model.dto.UserAccountUpdateRequest;
 import com.ulutman.model.entities.UserAccount;
 import com.ulutman.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,29 +9,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/user-accounts")
 public class UserAccountController {
     @Autowired
-    private  ProfileService userAccountService;
+    private ProfileService userAccountService;
 
     @PostMapping
-    public ResponseEntity<UserAccount> createUserAccount(@RequestParam Long userId,
-                                                         @RequestParam String username,
-                                                         @RequestParam String lastName,
-                                                         @RequestParam String phoneNumber,
-                                                         @RequestParam String emailAddress) {
-        UserAccount userAccount = userAccountService.createUserAccount(userId, username, lastName, phoneNumber, emailAddress);
+    public ResponseEntity<UserAccount> createUserAccount(@RequestBody UserAccountCreateRequest request) {
+        UserAccount userAccount = userAccountService.createUserAccount(
+                request.getUserId(),
+                request.getUsername(),
+                request.getLastName(),
+                request.getPhoneNumber(),
+                request.getEmailAddress()
+        );
         return new ResponseEntity<>(userAccount, HttpStatus.CREATED);
     }
 
     @PutMapping("/{userId}")
     public ResponseEntity<UserAccount> updateUserAccount(@PathVariable Long userId,
-                                                         @RequestParam String username,
-                                                         @RequestParam String lastName,
-                                                         @RequestParam String phoneNumber,
-                                                         @RequestParam String emailAddress) {
-        UserAccount userAccount = userAccountService.updateUserAccount(userId, username, lastName, phoneNumber, emailAddress);
+                                                         @RequestBody UserAccountUpdateRequest request) {
+        UserAccount userAccount = userAccountService.updateUserAccount(
+                userId,
+                request.getUsername(),
+                request.getLastName(),
+                request.getPhoneNumber(),
+                request.getEmailAddress()
+        );
         return new ResponseEntity<>(userAccount, HttpStatus.OK);
     }
 }
+
