07ee4e41c421ff2293c7d32bc983f257699efb4f
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-10-12 16:26:16 +0600

    Добавлен инициализатор администратора и исправлены импорты

--- src/main/java/com/ulutman/service/AdminService.java
+++ src/main/java/com/ulutman/service/AdminService.java
@@ -6,6 +6,7 @@ import com.ulutman.model.dto.AuthResponse;
 import com.ulutman.model.entities.Favorite;
 import com.ulutman.model.entities.User;
 import com.ulutman.model.enums.Role;
+import com.ulutman.model.enums.Status;
 import com.ulutman.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
 import java.time.LocalDate;
 
 @Service
@@ -23,15 +25,21 @@ import java.time.LocalDate;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AdminService {
 
-    @PostConstruct
-    public void init() {
-
-        System.out.println("AdminService инициализирован!");
-    }
-
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final AuthMapper authMapper;
+
+    @PostConstruct
+    public void initAdmin() {
+        User user = new User();
+        user.setName("Admin");
+        user.setEmail("admin@gmail.com");
+        user.setStatus(Status.АКТИВНЫЙ);
+        user.setRole(Role.ADMIN);
+        user.setPassword(passwordEncoder.encode("admin123"));
+        userRepository.save(user);
+    }
+
 
     public AuthResponse saveAdmin(AuthRequest request) {
         User user = authMapper.mapToEntity(request);
