38c112155201512dfef7d27cda0229c30ebd1f7e
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-10-15 18:13:37 +0600

    commit

--- src/main/java/com/ulutman/controller/S3Controller.java
+++ src/main/java/com/ulutman/controller/S3Controller.java
@@ -2,9 +2,7 @@ package com.ulutman.controller;
 
 import com.ulutman.service.S3Service;
 import io.jsonwebtoken.io.IOException;
-import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,7 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,8 +36,8 @@ public class S3Controller {
     @Operation(summary = "Загрузить файлы в AWS")
     @ApiResponse(responseCode = "201", description = "Uploads the files to AWS and returns the URLs of the uploaded files")
     @PostMapping("/upload")
-//    @ApiParam(value = "Файлы для загрузки", required = true)
-    public ResponseEntity<List<String>> uploadFiles(@RequestPart("files") MultipartFile[] files) {
+    public ResponseEntity<List<String>> uploadFiles(
+            @RequestPart(value = "files", required = true) MultipartFile[] files) {
         Map<String, Path> filesToUpload = new HashMap<>();
 
         for (MultipartFile file : files) {
@@ -56,11 +53,34 @@ public class S3Controller {
                 throw new RuntimeException(e);
             }
         }
-
         List<String> fileUrls = s3Service.uploadFiles(filesToUpload);
 
         return ResponseEntity.ok(fileUrls);
     }
+
+//    @PostMapping("/upload")
+////    @ApiParam(value = "Файлы для загрузки", required = true)
+//    public ResponseEntity<List<String>> uploadFiles(@RequestPart("files") MultipartFile[] files) {
+//        Map<String, Path> filesToUpload = new HashMap<>();
+//
+//        for (MultipartFile file : files) {
+//            try {
+//                // Сохраняем файл на временную директорию
+//                Path tempFilePath = Files.createTempFile(file.getOriginalFilename(), null);
+//                file.transferTo(tempFilePath.toFile()); // Перемещаем файл во временное место
+//                filesToUpload.put(file.getOriginalFilename(), tempFilePath);
+//            } catch (IOException e) {
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                        .body(List.of("Ошибка при сохранении файла: " + e.getMessage()));
+//            } catch (java.io.IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        List<String> fileUrls = s3Service.uploadFiles(filesToUpload);
+//
+//        return ResponseEntity.ok(fileUrls);
+//    }
 
 
 //    @Operation(summary = "Загрузить файлы в AWS")
@@ -102,7 +122,6 @@ public class S3Controller {
 //    }
 
 
-
 //    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 //    public ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file) {
 //        String fileName = file.getOriginalFilename();
@@ -124,4 +143,5 @@ public class S3Controller {
 //            throw new RuntimeException(e);
 //        }
 //    }
+
 }
