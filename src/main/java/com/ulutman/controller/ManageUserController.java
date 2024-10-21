df2938445826af384f9524e7db6d1d5b46678fc5
bakytzamalzorobekova <shamuratovna89@gmail.com>
2024-10-21 20:49:09 +0600

    updatea manage users

--- src/main/java/com/ulutman/controller/ManageUserController.java
+++ src/main/java/com/ulutman/controller/ManageUserController.java
@@ -1,7 +1,6 @@
 package com.ulutman.controller;
 
-import com.ulutman.model.dto.AuthRequest;
-import com.ulutman.model.dto.AuthResponse;
+import com.ulutman.exception.NotFoundException;
 import com.ulutman.model.dto.UserResponse;
 import com.ulutman.model.enums.Role;
 import com.ulutman.model.enums.Status;
@@ -12,7 +11,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,44 +28,27 @@ public class ManageUserController {
     private final ManageUserService manageUserService;
 
 
-    @Operation(summary = "Add a user")
-    @ApiResponse(responseCode = "201", description = "User added successfully")
-    @PostMapping("/addUsers")
-    public ResponseEntity<AuthResponse> save(@RequestBody AuthRequest authRequest) {
-        AuthResponse response = manageUserService.save(authRequest);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-    }
-
     @Operation(summary = "Get all users")
     @ApiResponse(responseCode = "201", description = "Return list if users")
     @GetMapping("/getAll")
-    public List<AuthResponse> getAll() {
+    public List<UserResponse> getAll() {
         return manageUserService.getAllUsers();
     }
 
     @Operation(summary = "Get user by id")
     @ApiResponse(responseCode = "201", description = "User found")
     @GetMapping("/getById/{id}")
-    public AuthResponse getById(@PathVariable Long id) {
+    public UserResponse getById(@PathVariable Long id) {
         return manageUserService.getUserById(id);
-    }
-
-
-    @Operation(summary = "Delete user by id")
-    @ApiResponse(responseCode = "201", description = "Deleted user by id successfully")
-    @DeleteMapping("/deleteById{id}")
-    public String deleteById(@PathVariable Long id) {
-        manageUserService.deleteUserById(id);
-        return "Пользователь успешно удален";
     }
 
     @Operation(summary = "Update user status")
     @ApiResponse(responseCode = "201", description = "Updated user status successfully")
     @PutMapping("/{id}/status")
-    public ResponseEntity<AuthResponse> updateUserStatus(@PathVariable Long id,
+    public ResponseEntity<UserResponse> updateUserStatus(@PathVariable Long id,
                                                          @RequestParam Status newStatus) {
-        AuthResponse authResponse = manageUserService.updateUserStatus(id, newStatus);
-        return ResponseEntity.ok(authResponse);
+        UserResponse userResponse = manageUserService.updateUserStatus(id, newStatus);
+        return ResponseEntity.ok(userResponse);
     }
 
     @Operation(summary = "Filter users by criteria")
@@ -87,7 +68,19 @@ public class ManageUserController {
     @Operation(summary = "Reset filters users")
     @ApiResponse(responseCode = "201", description = "Users filters successfully reset")
     @GetMapping("/resetFilter")
-    public List<AuthResponse> resetFilter() {
+    public List<UserResponse> resetFilter() {
         return manageUserService.getAllUsers();
+    }
+
+    @Operation(summary = "Delete user by id")
+    @ApiResponse(responseCode = "201", description = "Deleted user by id successfully")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id) {
+        try {
+            manageUserService.deleteUserById(id);
+            return ResponseEntity.ok("Пользователь успешно удален "+" " + id); // Возвращаем 200 OK с сообщением
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build(); // Возвращаем 404 Not Found, если пользователь не найден
+        }
     }
 }
