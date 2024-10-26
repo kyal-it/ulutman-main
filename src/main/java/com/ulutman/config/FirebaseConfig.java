ae9f3f8f480ba996637ed517b2bb1f5a6d854b7f
duisheevvva <duisheeva.aizat@icloud.com>
2024-10-26 13:46:39 +0600

    commit1

--- src/main/java/com/ulutman/config/FirebaseConfig.java
+++ src/main/java/com/ulutman/config/FirebaseConfig.java
@@ -3,25 +3,25 @@ package com.ulutman.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import io.jsonwebtoken.io.IOException;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
-    public FirebaseApp initializeFirebase() throws IOException, java.io.IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("serviceAccountKey.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+    @PostConstruct
+    void init() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials.
+                fromStream(new ClassPathResource("serviceAccountKey.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
                 .build();
-
-        return FirebaseApp.initializeApp(options);
+        FirebaseApp.initializeApp(firebaseOptions);
     }
 }
