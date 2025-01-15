8a2585538f3962a300873ae040900ffb3052e5dd
bakytzamalzorobekova <shamuratovna89@gmail.com>
2025-01-15 13:23:10 +0300

    change LocalConfig

--- src/main/java/com/ulutman/config/LocaleConfig.java
+++ src/main/java/com/ulutman/config/LocaleConfig.java
@@ -7,17 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
 public class LocaleConfig extends AcceptHeaderLocaleResolver {
-
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setUseCodeAsDefaultMessage(true); // Показать ключ сообщения, если перевод не найден
+        messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
     }
 
@@ -25,8 +26,25 @@ public class LocaleConfig extends AcceptHeaderLocaleResolver {
     public Locale resolveLocale(HttpServletRequest request) {
         String acceptLanguage = request.getHeader("Accept-Language");
         System.out.println("Accept-Language: " + acceptLanguage); // Логирование
-        return acceptLanguage == null || acceptLanguage.isEmpty()
-                ? Locale.forLanguageTag("ru") // Язык по умолчанию — русский
-                : Locale.forLanguageTag(acceptLanguage);
+
+        List<Locale> supportedLocales = Arrays.asList(
+                Locale.forLanguageTag("ru"),
+                Locale.forLanguageTag("en"),
+                Locale.forLanguageTag("tr"),
+                Locale.forLanguageTag("ky"),
+                Locale.forLanguageTag("tj"),
+                Locale.forLanguageTag("uz")
+        );
+
+        if (acceptLanguage == null || acceptLanguage.isEmpty()) {
+            return Locale.forLanguageTag("ru");
+        }
+
+        Locale requestedLocale = Locale.forLanguageTag(acceptLanguage);
+        if (supportedLocales.contains(requestedLocale)) {
+            return requestedLocale;
+        }
+
+        return Locale.forLanguageTag("ru");
     }
 }
